f78beece5445326a62a6db90208aca02e33638a7
Monye Jude <chuksjude7@gmail.com>
2023-02-16 09:13:05 -0800

    4-puts.c created and content added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A C program to displays a line with put function
+ * Return: 0 (program is successful when it returns 0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
