744d6a31af39a620645a6582e2f353033c17b0ed
Monye Jude <chuksjude7@gmail.com>
2023-02-16 09:24:03 -0800

    5-printf.c created and content added as directed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - A C program that displays a line using the printf function
+* Return: 0 (program is successful if it returns 0)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
