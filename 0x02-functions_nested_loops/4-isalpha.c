fa4788f9d6552c653448fac683d1808b741792b0
Monye Jude <chuksjude7@gmail.com>
2023-02-21 10:23:18 -0800

    4-isalpha.c task completed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _isalpha - checks for alphabet characters
+/**
+ * _isalpha - checks for alphabet characters
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 otherwise
  */
