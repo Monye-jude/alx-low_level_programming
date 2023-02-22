d30939af8f353b2e7377966e370673239ef280f7
Monye Jude <chuksjude7@gmail.com>
2023-02-21 16:18:25 -0800

    10-add.c task completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - To add two numbers together
+ * @n1: The first number to be added
+ * @n2: The second number to be added
+ * Return: Integer value of the result
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
