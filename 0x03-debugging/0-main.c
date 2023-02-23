6cbc5b40a41e88cbae06a0ed3fab8e3ef2c35d3b
Monye Jude <chuksjude7@gmail.com>
2023-02-23 07:25:10 -0800

    0-main.c, main.h files added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Returb: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	popsitive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+##Debugging in C
+Files are being generated including header files
+**Corrections** are to be made to the file when necessary
